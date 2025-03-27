0eb64f538cc729bc4efc0cc5384589334fb4f092
Hiep Chan <hiep.lchan@gmail.com>
2025-03-27 22:26:00 +0700

    Update Entity.cs

--- Poko A Magical Wish/Assets/Scripts/Ingame/Entity.cs
+++ Poko A Magical Wish/Assets/Scripts/Ingame/Entity.cs
@@ -8,8 +8,10 @@ public class Entity : MonoBehaviour
     [SerializeField, Child] public Rigidbody Rigidbody;
     [SerializeField, Self] public HealthComp HealthComp;
 
+#if UNITY_EDITOR
     void OnValidate()
     {
         this.ValidateRefs();
     }
+#endif
 }
