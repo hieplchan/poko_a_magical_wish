fd11d9c14f1a150d1dc77c69ad5e3b754ac3bbec
Hiep Chan <hiep.lchan@gmail.com>
2025-04-01 20:21:01 +0700

    minor change

--- Poko A Magical Wish/Assets/Scripts/Ingame/Player/PlayerControllerComponent.cs
+++ Poko A Magical Wish/Assets/Scripts/Ingame/Player/PlayerControllerComponent.cs
@@ -1,6 +1,20 @@
+using System;
 using KBCore.Refs;
 using UnityEngine;
 
 public class PlayerControllerComponent : MonoBehaviour {
-    [SerializeField, Self] private Player _player;
+    [SerializeField, Self] private readonly Rigidbody rbBody;
+    [SerializeField, Anywhere] private readonly InputReader input;
+
+    public Vector3 Movement { get; private set; }
+
+    private Transform mainCameraTransform;
+
+    private void OnEnable() {
+        mainCameraTransform = Camera.main.transform;
+    }
+
+    private void Update() {
+        Movement = new Vector3(input.Direction.x, 0, input.Direction.y);
+    }
 }
